5426acd9fcfbe98ca6262e525fa14446757f38da
Joshua Benjamin <jbenj@sas.upenn.edu>
2021-11-19 13:22:44 -0500

    multiple changes to format and blog page

--- header.php
+++ header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 
 <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+        <!--THE META TAG MAKES THE PAGE MOVILE RESPONSIVE-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
-
-<body>
+<body <?php body_class(); ?>>
     <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left">
@@ -15,13 +17,23 @@
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
             <div class="site-header__menu group">
                 <nav class="main-navigation">
+                    <!--the following is dynamic through WP-->
+                    <!--
+                    <?php 
+                      wp_nav_menu(array(
+                          'theme_location' => 'headerMenuLocation'
+                      ));
+                    ?>
+                    -->
+                    <!--the following is baked in HTML, not dynamic, but dropping into PHP offers more fine-grain control of something as simple as the page or parent page link being a different color, i.e., current-menu-item-->
                     <ul>
-                        <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-                        <li><a href="<?php echo site_url('/programs') ?>">Programs</a></li>
-                        <li><a href="<?php echo site_url('/events') ?>">Events</a></li>
-                        <li><a href="<?php echo site_url('/campuses') ?>">Campuses</a></li>
-                        <li><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
+                        <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                        <li <?php if (is_page('programs') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>"><a href="<?php echo site_url('/programs') ?>">Programs</a></li>
+                        <li <?php if (is_page('events') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>"><a href="<?php echo site_url('/events') ?>">Events</a></li>
+                        <li <?php if (is_page('campuses') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>"><a href="<?php echo site_url('/campuses') ?>">Campuses</a></li>
+                        <li <?php if (is_page('blog') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>"><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
                     </ul>
+
                 </nav>
                 <div class="site-header__util">
                     <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
