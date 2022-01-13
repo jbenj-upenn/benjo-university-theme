7a153f52a155f44856b1aa3e09559d8d8e98d4b8
Joshua Benjamin <jbenj@sas.upenn.edu>
2022-01-13 17:29:32 -0500

    worked with customizing events page for specific events in the future and deleting those that have past

--- front-page.php
+++ front-page.php
@@ -20,7 +20,7 @@
     <?php
       $today = date('Ymd');
       $homepageEvents = new WP_Query(array(
-        'posts_per_page' => -1,
+        'posts_per_page' => 2,
         'post_type' => 'event',
         'meta_key' => 'event_date',
         'orderby' => 'meta_value_num',
